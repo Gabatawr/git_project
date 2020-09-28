f7b3f3ec88f1e91c0cc50f6518426afd7d4d4372
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:43:06 +0300

    tabs

--- Task list/Task list/SortList.h
+++ Task list/Task list/SortList.h
@@ -8,9 +8,9 @@ class SortList
 protected://---------------------------------------
     std::multimap<std::string, Task*> _storageList;
 public://------------------------------------------
-    void Print       ();
-    bool Filter      (std::string key);
+    void Print  ();
+    bool Filter (std::string key);
 	
-    void Add         (std::string, Task*);
-    void Del         (int id);
+    void Add    (std::string, Task*);
+    void Del    (int id);
 };//-----------------------------------------------
