0471b19d7353d9a4a7ab8a73825641ea14ae083f
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:38:48 +0300

    tabs

--- Task list/Task list/Task.h
+++ Task list/Task list/Task.h
@@ -6,37 +6,37 @@
 class Task : public SaveLoad
 {
 private://----------------------------------------------------------------------------
-	static int          IDCounter;
+    static    int            IDCounter;
 	
-           std::string _text;
-	       std::string _tag;
+              std::string   _text;
+              std::string   _tag;
 	
-	       int         _priority;
-	       DateTime*   _dateTime;
+              int           _priority;
+              DateTime*     _dateTime;
 
-	       int         _ID;
-	       bool        _done;
+              int           _ID;
+              bool          _done;
 private://----------------------------------------------------------------------------
-	             Task         (std::string& text, std::string& tag,
-			                   int priority, DateTime* dateTime,
-					           int id = 0, bool done = false);
+              Task          (std::string& text, std::string& tag,
+                             int priority, DateTime* dateTime,
+                             int id = 0, bool done = false);
 public://-----------------------------------------------------------------------------
-	static Task* GetTask      (std::string text, std::string tag,
-		                       std::string priority, std::string date, std::string time,
-		                       std::string  id = "", std::string  done = "");
+  static      Task* GetTask (std::string text, std::string tag,
+                             std::string priority, std::string date, std::string time,
+                             std::string  id = "", std::string  done = "");
 
-	static int   GetIDCounter ();
-	static void  SetIDCounter (int);
-	       int   GetID        () const;
+  static int  GetIDCounter  ();
+  static void SetIDCounter  (int);
+         int  GetID         () const;
 	
-	       void  Print        () const;
+         void Print         () const;
 	       
-	       void  Edit         ();
-	       void  Done         ();
+         void Edit          ();
+         void Done          ();
 
-	       void  Save         () override;
-	       void  Load         () override;
+         void Save          () override;
+         void Load          () override;
 
-		        ~Task         () override;
+             ~Task          () override;
 };//----------------------------------------------------------------------------------
 
