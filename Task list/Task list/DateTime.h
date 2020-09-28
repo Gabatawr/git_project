d9d03d929595562b8c1167d7e307ddcc546cedcb
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:31:46 +0300

    tabs

--- Task list/Task list/DateTime.h
+++ Task list/Task list/DateTime.h
@@ -5,20 +5,19 @@ class DateTime
 {//----------------------------------------------------------
 public: enum Type { Date, Time };
 private://---------------------------------------------------
-	int _month;
-	int _day;
-	int _year;
+        int _month;
+        int _day;
+        int _year;
 
-	int _hour;
-	int _min;
+        int _hour;
+        int _min;
 	
 private://---------------------------------------------------
-	    bool        Check    (Type) const;
+        bool        Check    (Type) const;
 public://----------------------------------------------------
-	                DateTime ();
-	                DateTime (std::string, std::string = "");
+                    DateTime ();
+                    DateTime (std::string, std::string = "");
 	
-	    bool        Replace  (std::string&, Type);
+        bool        Replace  (std::string&, Type);
         std::string toString (Type) const;
 };//---------------------------------------------------------
-
