5c6686d727f82b077129babef9ec7bdfad4f7443
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:39:56 +0300

    tabs

--- Task list/Task list/TaskBox.h
+++ Task list/Task list/TaskBox.h
@@ -6,24 +6,24 @@
 
 class TaskBox : public SaveLoad
 {//--------------------------------------------------
-public: enum Type { Tag, Priority, Date, Unknown };
+public:  enum Type { Tag, Priority, Date, Unknown };
 private://-------------------------------------------
-	SortList*        _sortList[3];
-	std::list<Task*> _taskList;
+         SortList*        _sortList[3];
+         std::list<Task*> _taskList;
 public://--------------------------------------------
-	     TaskBox ();
+         TaskBox ();
 	
-	void Print   (Type sortType);
-	bool Filter  (Type filterType, std::string& key);
+    void Print   (Type sortType);
+    bool Filter  (Type filterType, std::string& key);
 	
-	void Add     ();
-	void Del     (int id);
-	void Edit    (int id);
-	void Done    (int id);
+    void Add     ();
+    void Del     (int id);
+    void Edit    (int id);
+    void Done    (int id);
 	
-	void Save    () override;
-	void Load    () override;
+    void Save    () override;
+    void Load    () override;
 	
-	    ~TaskBox () override;
+        ~TaskBox () override;
 };//-------------------------------------------------
 
