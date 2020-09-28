9ee1cdf1bb5a1277bcbcb96c47ca6a691efdfb28
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:42:00 +0300

    tabs

--- Task list/Task list/Task.h
+++ Task list/Task list/Task.h
@@ -5,38 +5,38 @@
 
 class Task : public SaveLoad
 {
-private://----------------------------------------------------------------------------
-    static    int            IDCounter;
+private://------------------------------------------------------------------------------
+    static int          IDCounter;
 	
-              std::string   _text;
-              std::string   _tag;
+           std::string _text;
+           std::string _tag;
 	
-              int           _priority;
-              DateTime*     _dateTime;
+           int         _priority;
+           DateTime*   _dateTime;
 
-              int           _ID;
-              bool          _done;
-private://----------------------------------------------------------------------------
-              Task          (std::string& text, std::string& tag,
-                             int priority, DateTime* dateTime,
-                             int id = 0, bool done = false);
-public://-----------------------------------------------------------------------------
-  static      Task* GetTask (std::string text, std::string tag,
-                             std::string priority, std::string date, std::string time,
-                             std::string  id = "", std::string  done = "");
+           int         _ID;
+           bool        _done;
+private://------------------------------------------------------------------------------
+                Task          (std::string& text, std::string& tag,
+                               int priority, DateTime* dateTime,
+                               int id = 0, bool done = false);
+public://-------------------------------------------------------------------------------
+    static      Task* GetTask (std::string text, std::string tag,
+                               std::string priority, std::string date, std::string time,
+                               std::string  id = "", std::string  done = "");
 
-  static int  GetIDCounter  ();
-  static void SetIDCounter  (int);
-         int  GetID         () const;
+    static int  GetIDCounter  ();
+    static void SetIDCounter  (int);
+           int  GetID         () const;
 	
-         void Print         () const;
+           void Print         () const;
 	       
-         void Edit          ();
-         void Done          ();
+           void Edit          ();
+           void Done          ();
 
-         void Save          () override;
-         void Load          () override;
+           void Save          () override;
+           void Load          () override;
 
-             ~Task          () override;
-};//----------------------------------------------------------------------------------
+               ~Task          () override;
+};//------------------------------------------------------------------------------------
 
