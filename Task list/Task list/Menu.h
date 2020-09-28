b6e25c8da0e9474d4918f33b01b575501a7ad8a4
Artem <51173038+Gabatawr@users.noreply.github.com>
2020-09-28 11:30:31 +0300

    tabs

--- Task list/Task list/Menu.h
+++ Task list/Task list/Menu.h
@@ -4,28 +4,28 @@
 class Menu
 {
 private://--------------------------------------------------------------------
-	static Menu*         _menu;
-	       TaskBox*      _taskBox;
-	       TaskBox::Type _printMode;
+    static Menu*         _menu;
+           TaskBox*      _taskBox;
+           TaskBox::Type _printMode;
 private://--------------------------------------------------------------------
-	                     Menu         (const char* pathFile);
-	
-	       void          Help         ();
-		   void          SetPrintMode (std::string& command);
-	
-	       TaskBox::Type PreFilter    (std::string& command);
-	
-	       TaskBox::Type ParseKey     (std::string& command);
-	       bool          FindStr      (std::string& command, const char* key);
+                         Menu         (const char* pathFile);
 
-		   bool          Exit         ();
+           void          Help         ();
+           void          SetPrintMode (std::string& command);
+
+           TaskBox::Type PreFilter    (std::string& command);
+
+           TaskBox::Type ParseKey     (std::string& command);
+           bool          FindStr      (std::string& command, const char* key);
+
+           bool          Exit         ();
 public://---------------------------------------------------------------------
-	static Menu*         Loading      (const char* pathFile);
-	       void          Run          ();
+    static Menu*         Loading      (const char* pathFile);
+           void          Run          ();
 
-	static void          FixByDate    (std::string& date);
-	static std::string   IntToStr     (int number, bool frontZero = false);
-	static int           StrToInt     (std::string& str, int numInRow = 1);
-	
-	                    ~Menu         ();
+    static void          FixByDate    (std::string& date);
+    static std::string   IntToStr     (int number, bool frontZero = false);
+    static int           StrToInt     (std::string& str, int numInRow = 1);
+
+                        ~Menu         ();
 };//--------------------------------------------------------------------------
